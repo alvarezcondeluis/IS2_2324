0ad5fd8d0a29ccacc7a58e3606d07e4b14ab5e73
alvarezcondeluis <alvarezcondeluis@gmail.com>
2024-03-06 12:55:47 +0100

    GestionEmpleados

--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
@@ -2,6 +2,7 @@ package es.unican.is2.business;
 
 
 import es.unican.is2.domain.Empleado;
+import es.unican.is2.domain.Tienda;
 import es.unican.is2.exceptions.DataAccessException;
 import es.unican.is2.exceptions.OperacionNoValidaException;
 import es.unican.is2.interfaces.IEmpleadosDAO;
@@ -10,14 +11,27 @@ import es.unican.is2.interfaces.ITiendasDAO;
 
 
 public class GestionEmpleados implements IGestionEmpleados{
-
+	
+	private ITiendasDAO tiendas;
+	private IEmpleadosDAO empleados;
+	
 	public GestionEmpleados(ITiendasDAO tiendasDAO, IEmpleadosDAO empleadosDAO) {
-		// TODO Auto-generated constructor stubsfd
+		this.tiendas = tiendasDAO;
+		this.empleados = empleadosDAO;
 	}
 
 	public Empleado nuevoEmpleado(Empleado e, String nombre) throws OperacionNoValidaException, DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+		Tienda t = tiendas.tiendaPorNombre(nombre);
+		if (t == null) {
+			return null;
+		}
+		if (empleados.empleado(e.getDNI()) != null) {
+			throw new OperacionNoValidaException("Empleado ya existe");
+		};
+		t.getEmpleados().add(e);
+		empleados.crearEmpleado(e);
+		
+		return e;
 	}
 
 	public Empleado eliminarEmpleado(String dni, String nombre) throws OperacionNoValidaException, DataAccessException {
@@ -27,8 +41,20 @@ public class GestionEmpleados implements IGestionEmpleados{
 
 	public boolean trasladarEmpleado(String dni, String actual, String destino)
 			throws OperacionNoValidaException, DataAccessException {
-		// TODO Auto-generated method stub
-		return false;
+		Empleado e = empleados.empleado(dni);
+		Tienda actualTienda = tiendas.tiendaPorNombre(actual);
+		Tienda destinoTienda = tiendas.tiendaPorNombre(destino);
+		
+		if (e == null || actualTienda == null || destinoTienda == null) {
+			return false;
+		}
+		
+		if (!actualTienda.getEmpleados().contains(e)) {
+			throw new OperacionNoValidaException("El empleado no pertenece a la tienda");
+		}
+		actualTienda.getEmpleados().remove(e);
+		destinoTienda.getEmpleados().add(e);
+		return true;
 	}
 
 	public Empleado empleado(String dni) throws DataAccessException {
