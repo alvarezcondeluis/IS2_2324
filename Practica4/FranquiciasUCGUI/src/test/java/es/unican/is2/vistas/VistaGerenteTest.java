bfd62b726ad261e1e4f5c3f10e2524a8e91c41ed
adrigonzaleezz <adrian2000gonzalez@gmail.com>
2024-03-29 15:50:22 +0100

    Creacion de VistaGerenteTest

---
+++ Practica4/FranquiciasUCGUI/src/test/java/es/unican/is2/vistas/VistaGerenteTest.java
@@ -0,0 +1,17 @@
+package es.unican.is2.vistas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.fest.swing.fixture.FrameFixture;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class VistaGerenteTest {
+	
+	private FrameFixture demo;
+	
+	@BeforeAll
+	public void setUp() {
+	
+	}
+}
