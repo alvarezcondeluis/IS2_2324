c55aa69c70821eff1bf2c7f4b0e5621bb20227b8
adrigonzaleezz <adrian2000gonzalez@gmail.com>
2024-04-12 15:06:11 +0200

    Anhado ListaOrdenadaAcotada y elimino jar

--- Practica4/ListaOrdenadaAcotada/src/test/java/es/unican/is2/listaordenada/ListaOrdenadaAcotadaTest.java
+++ Practica4/ListaOrdenadaAcotada/src/test/java/es/unican/is2/listaordenada/ListaOrdenadaAcotadaTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import es.unican.is2.listaOrdenadaAcotada.ListaOrdenadaAcotada;
+import es.unican.is2.listaordenada.ListaOrdenadaAcotada;
 
 class ListaOrdenadaAcotadaTest {
 	
